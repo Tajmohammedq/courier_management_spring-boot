b3c3e8e98449fd4d553483874ab9c9cc4cc1d9c6
Tajmohammedq <mohammedtaj36912@gmail.com>
2026-05-14 10:41:11 +0530

    Added completed and cancelled orders services

--- src/main/java/jpapackage/repo/CancelledOrdersRepo.java
+++ src/main/java/jpapackage/repo/CancelledOrdersRepo.java
@@ -1,9 +1,11 @@
 package jpapackage.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import jpapackage.entity.CancelledOrders;
 
 public interface CancelledOrdersRepo extends JpaRepository<CancelledOrders, Long>{
-
+	List<CancelledOrders> findByEmailIgnoreCase(String email);
 }
--- src/main/java/jpapackage/service/Implementation/JwtServiceImplementations.java
+++ src/main/java/jpapackage/service/Implementation/JwtServiceImplementations.java
@@ -20,7 +20,7 @@ public class JwtServiceImplementations implements JWTSERVICE {
 	public String generateToken(UserDetails userdetails) {
 		return Jwts.builder().setSubject(userdetails.getUsername())
 				.setIssuedAt(new Date(System.currentTimeMillis()))
-				.setExpiration(new Date(System.currentTimeMillis() + 1000*60*24))
+				.setExpiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24))
 				.signWith(getKey(),SignatureAlgorithm.HS256)
 				.compact();		
 		
